8da84755333c6cd7dd16af15a878b868846f54ff
Jsung <jusng@outlook.com>
2017-08-18 20:37:41 +0800

    添加了缩略图

--- src/js/index.js
+++ src/js/index.js
@@ -91,18 +91,18 @@ var KinerLottery = new KinerLottery({
 });
 
 /** 屏蔽分享 */
-function onBridgeReady() {
-    WeixinJSBridge.call('hideOptionMenu');
-    }
+// function onBridgeReady() {
+//     WeixinJSBridge.call('hideOptionMenu');
+//     }
     
     
-    if (typeof WeixinJSBridge == "undefined") {
-    if (document.addEventListener) {
-    document.addEventListener('WeixinJSBridgeReady', onBridgeReady, false);
-    } else if (document.attachEvent) {
-    document.attachEvent('WeixinJSBridgeReady', onBridgeReady);
-    document.attachEvent('onWeixinJSBridgeReady', onBridgeReady);
-    }
-    } else {
-    onBridgeReady();
-    }
+//     if (typeof WeixinJSBridge == "undefined") {
+//     if (document.addEventListener) {
+//     document.addEventListener('WeixinJSBridgeReady', onBridgeReady, false);
+//     } else if (document.attachEvent) {
+//     document.attachEvent('WeixinJSBridgeReady', onBridgeReady);
+//     document.attachEvent('onWeixinJSBridgeReady', onBridgeReady);
+//     }
+//     } else {
+//     onBridgeReady();
+//     }
