7caaa774cf303b432f680ce02d137b7bef6c02f2
Jsung <jusng@outlook.com>
2017-08-19 13:36:12 +0800

    优化业务逻辑

--- src/fs/fwrite.php
+++ src/fs/fwrite.php
@@ -1,7 +1,7 @@
 <?php
 
-$path ="../json/prizeInfo.json";
-$content = '"{"name":"'.$_GET['name'].'","age":"'.$_GET['age'].'"}",';
+$path = '../json/prizeInfo.json';
+$content = '"{"name":"'.$_GET['name'].'","mobile":"'.$_GET['mobile'].'","wechat":"'.$_GET['wechat'].'","address":"'.$_GET['address'].'"}",';
 file_put_contents($path,$content,FILE_APPEND);
 if (file_exists($path))
  {echo "ok";}
--- src/js/index.js
+++ src/js/index.js
@@ -1,119 +1,163 @@
  /**
-     * 根据转盘旋转角度判断获得什么奖品
-     * @param deg
-     * @returns {*}
-     */
-    var arr = []
-    function pushArr(code,len){
-        for(var i=0;i<len;i++){
-            arr.push(code)
-        }
-    }
-    pushArr(1,2)
-    pushArr(2,200)
-    pushArr(3,300)
-    pushArr(4,498)
+  * 根据转盘旋转角度判断获得什么奖品
+  * @param deg
+  * @returns {*}
+  */
+ var arr = []
 
-var whichAward = function(deg) {
-    if(deg>=0&&deg<90){
-        $.get('./fs/mwrite.php?extend=x',function(res){
-            console.log(res);
-        })
-        // $('.mask').css('display','block')
-        return 'one'
-    }else if(deg>=90&&deg<180){
+ function pushArr(code, len) {
+     for (var i = 0; i < len; i++) {
+         arr.push(code)
+     }
+ }
+ pushArr(1, 2)
+ pushArr(2, 200)
+ pushArr(3, 300)
+ pushArr(4, 498)
 
-
-        return 'tow'
-    }else if(deg>=180&&deg<270){
-
-
-        return 'three'
-    }else{
-
+ var whichAward = function (deg) {
+     console.log(deg);
+     if (deg >= 0 && deg < 90) {
+        $('.mask').show()
         
-        return 'four'
-    }
-}
+                 console.log('4等奖');
+                 
+                 return 'four'
+         
+     } else if (deg >= 90 && deg < 180) {
 
-function randomDeg(){
-    var tp = arr[Math.floor(Math.random()*arr.length)]
-    console.log('tp:'+tp);
-    var tl = Math.floor(Math.random()*90)
-    if(tp==1){
-        return 0+tl
-    }else if(tp==2){
-        return 90+tl
-    }else if(tp==3){
-        return 180+tl
-    }else if(tp==4){
-        return 270+tl
-    }
-}
+         $('.mask').show()
 
-var KinerLottery = new KinerLottery({
-    rotateNum: 8, //转盘转动圈数
-    body: "#box", //大转盘整体的选择符或zepto对象
-    direction: 0, //0为顺时针转动,1为逆时针转动
+         console.log('2等奖');
+         
+         return 'tow'
+     } else if (deg >= 180 && deg < 270) {
 
-    disabledHandler: function(key) {
+         $('.mask').show()
 
-        switch (key) {
-            case "noStart":
-                alert("活动尚未开始");
-                break;
-            case "completed":
-                alert("活动已结束");
-                break;
-        }
+         console.log('3等奖');
+         
+         return 'three'
+     } else {
 
-    }, //禁止抽奖时回调
+       
 
-    clickCallback: function() {
-        var that = this
-        //此处访问接口获取奖品
-        $.get('./fs/mread.php',function(res){
-            if(res.length>=100){
-                arr.shift()
-                arr.shift()
-            }
-            that.goKinerLottery(randomDeg());
-        })
+         console.log('一等奖');
+         $.get('./fs/mwrite.php?extend=x', function (res) {
+             console.log(res);
+         })
+         $('.mask').show()
+         $('.submit').show()
+
+         return 'one'
+     }
+ }
+
+ function randomDeg() {
+     var tp = arr[Math.floor(Math.random() * arr.length)]
+     console.log('tp:' + tp);
+     var tl = Math.floor(Math.random() * 90)
+     if (tp == 1) {
+         return 0 + tl
+     } else if (tp == 2) {
+         return 90 + tl
+     } else if (tp == 3) {
+         return 180 + tl
+     } else if (tp == 4) {
+         console.log(270 , tl);
+         console.log(270 + tl);
+         return 270 + tl
+     }
+ }
+
+ var KinerLottery = new KinerLottery({
+     rotateNum: 8, //转盘转动圈数
+     body: "#box", //大转盘整体的选择符或zepto对象
+     direction: 0, //0为顺时针转动,1为逆时针转动
+
+     disabledHandler: function (key) {
+
+         switch (key) {
+             case "noStart":
+                 alert("活动尚未开始");
+                 break;
+             case "completed":
+                 alert("您已经参与过活动了");
+                 break;
+         }
+
+     }, //禁止抽奖时回调
+
+     clickCallback: function () {
+         var that = this
+         //此处访问接口获取奖品
+         $.get('./fs/mread.php', function (res) {
+             if (res.length >= 100) {
+                 arr.shift()
+                 arr.shift()
+             }
+             that.goKinerLottery(randomDeg());
+         })
+     }, //点击抽奖按钮,再次回调中实现访问后台获取抽奖结果,拿到抽奖结果后显示抽奖画面
 
 
-    }, //点击抽奖按钮,再次回调中实现访问后台获取抽奖结果,拿到抽奖结果后显示抽奖画面
+     KinerLotteryHandler: function (deg) {
+
+         whichAward(deg)
+
+        //  localStorage.setItem('flang', 'ok')
+        //  document.querySelector('.KinerLotteryBtn').classList.add('completed')
+        //  document.querySelector('.KinerLotteryBtn').classList.remove('start')
+
+     } //抽奖结束回调
+ });
+
+ if (localStorage.getItem('flang')) {
+     document.querySelector('.KinerLotteryBtn').classList.add('completed')
+     document.querySelector('.KinerLotteryBtn').classList.remove('start')
+ }
+
+ // 表单提交
+ $('.submit').on('submit', function (e) {
+     e.preventDefault();
+     $.ajax({
+         url: './fs/fwrite.php',
+         type: 'get',
+         data: $(this).serialize(),
+         success: function (res) {
+             console.log(res);
+         }
+     })
+     $('.mask').hide()
+ })
+
+ $('.mask').on('tap',function () {
+     $(this).hide()
+ })
+ $('.mask-close').on('tap', function () {
+     $('.mask').hide()
+ })
+// 阻止事件冒泡
+$('.mask-img').on('tap',function(e){
+    e.stopPropagation()
+})
 
 
 
-    KinerLotteryHandler: function(deg) {
 
-            // alert("恭喜您获得:" + whichAward(deg));
-            whichAward(deg)
-            localStorage.setItem('flang','ok')
-            document.querySelector('.KinerLotteryBtn').classList.add('completed')
-            document.querySelector('.KinerLotteryBtn').classList.remove('start')
-
-        } //抽奖结束回调
-});
+ /** 屏蔽分享 */
+ // function onBridgeReady() {
+ //     WeixinJSBridge.call('hideOptionMenu');
+ //     }
 
 
-if(localStorage.getItem('flang')){
-    document.querySelector('.KinerLotteryBtn').classList.add('completed')
-    document.querySelector('.KinerLotteryBtn').classList.remove('start')
-}
-/** 屏蔽分享 */
-// function onBridgeReady() {
-//     WeixinJSBridge.call('hideOptionMenu');
-//     }
-    
-    
-//     if (typeof WeixinJSBridge == "undefined") {
-//     if (document.addEventListener) {
-//     document.addEventListener('WeixinJSBridgeReady', onBridgeReady, false);
-//     } else if (document.attachEvent) {
-//     document.attachEvent('WeixinJSBridgeReady', onBridgeReady);
-//     document.attachEvent('onWeixinJSBridgeReady', onBridgeReady);
-//     }
-//     } else {
-//     onBridgeReady();
-//     }
+ //     if (typeof WeixinJSBridge == "undefined") {
+ //     if (document.addEventListener) {
+ //     document.addEventListener('WeixinJSBridgeReady', onBridgeReady, false);
+ //     } else if (document.attachEvent) {
+ //     document.attachEvent('WeixinJSBridgeReady', onBridgeReady);
+ //     document.attachEvent('onWeixinJSBridgeReady', onBridgeReady);
+ //     }
+ //     } else {
+ //     onBridgeReady();
+ //     }
